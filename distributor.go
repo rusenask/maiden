87d41a09dd4ad117a9b92bc8f62ce4f8af2768a3
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-10-21 19:29:00 +0100

    saving image to disk

--- distributor.go
+++ distributor.go
@@ -1,12 +1,18 @@
 package maiden
 
 import (
+	// "fmt"
+	"os"
+	"path/filepath"
+
 	docker "github.com/fsouza/go-dockerclient"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 // Distributor - placeholder for distributor
 type Distributor interface {
-	ShareImage() error
+	ShareImage(name string) error
 	PullImage() error
 }
 
@@ -18,6 +24,29 @@ func NewDefaultDistributor(client *docker.Client) *DefaultDistributor {
 	return &DefaultDistributor{client: client}
 }
 
-func (d *DefaultDistributor) ShareImage() error {
+func (d *DefaultDistributor) ShareImage(name string) error {
+	err := d.getImage(name)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func (d *DefaultDistributor) getImage(name string) error {
+	// checking whether we have this image
+	f, err := os.Create(filepath.Join("images", name))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("failed to create data torrent")
+	}
+	defer f.Close()
+
+	opts := docker.ExportImageOptions{
+		Name:         name,
+		OutputStream: f,
+	}
+
+	return d.client.ExportImage(opts)
 }
