8a73213eaf06e9a4e96a3dbcfd02ed7b74255d2f
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-10-21 19:28:50 +0100

    startup

--- cmd/maiden/main.go
+++ cmd/maiden/main.go
@@ -7,27 +7,58 @@ import (
 	"github.com/rusenask/maiden"
 
 	log "github.com/Sirupsen/logrus"
+
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+// docker required params
+const (
+	EnvDockerEndpoint = "DOCKER_ENDPOINT"
+	// EnvDockerRegistryEmail = "DOCKER_REGISTRY_EMAIL"
+	// EnvDockerRegistryAuth  = "DOCKER_REGISTRY_AUTH"
 )
 
 func main() {
 	dir := flag.String("dir", "", "directory from which to create a torrent file")
+	share := flag.String("share", "", "share selected image to your peers")
 	flag.Parse()
 
-	contents, err := maiden.Create(*dir)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-		}).Fatal("failed to create data torrent")
+	if *dir != "" {
+		contents, err := maiden.Create(*dir)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Fatal("failed to create data torrent")
+		}
+
+		f, err := os.Create("data.torrent")
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Fatal("failed to create data torrent")
+		}
+		defer f.Close()
+
+		f.Write(contents)
+		log.Infof("torrent for `%s` created", dir)
 	}
 
-	f, err := os.Create("data.torrent")
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-		}).Fatal("failed to create data torrent")
-	}
-	defer f.Close()
+	if *share != "" {
+		endpoint := "unix:///var/run/docker.sock"
+		if os.Getenv(EnvDockerEndpoint) != "" {
+			endpoint = os.Getenv(EnvDockerEndpoint)
+		}
 
-	f.Write(contents)
-	log.Infof("torrent for `%s` created", dir)
+		client, err := docker.NewClient(endpoint)
+		// client, err := docker.NewClientFromEnv()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Fatal("failed to get docker client from env")
+		}
+
+		distributor := maiden.NewDefaultDistributor(client)
+		distributor.ShareImage(*share)
+	}
+
 }
